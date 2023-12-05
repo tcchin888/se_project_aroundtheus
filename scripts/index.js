40de63e38f242af10b9e825252513dd3061a4030
tyler chin <chin.tyler10@gmail.com>
2023-12-04 23:21:26 -0600

    project 4.2

--- scripts/index.js
+++ scripts/index.js
@@ -20,7 +20,7 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
   {
-    title: "Lago Di Braies",
+    title: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
